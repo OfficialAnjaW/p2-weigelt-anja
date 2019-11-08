65eaaa1c84cdebdcd9254e647fc5ea016a3710eb
Anja Weigelt <aweigelt@ufl.edu>
2019-11-07 22:26:50 -0500

    Build, style and animate slide-in ad

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 
 // 2. POP-UP AD
 
+// Global Varibale
+
+const slideOutAd = document.getElementById('slideout-ad');
+
+// // Show Ad After 3 Seconds on Page
+
+setTimeout(() => {
+  slideOutAd.classList.remove('hide');
+  slideOutAd.classList.add('show');
+}, 2000);
+
+
 // 3. ACCORDION
 
 // Variables
